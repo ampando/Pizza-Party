bdd01ee51778a86c26487af20702db290a717709
Adrienne Matosich <amatosich@gmail.com>
2022-05-12 12:09:08 -0700

    update scripts.

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,16 @@
 // Business Logic
-function Pizza() {
-  this.pizzaParty = {};
-}
-function PizzaParty(toppings, size,) {
+function PizzaParty(toppings, size) {
     this.toppings = toppings;
     this.size = size; 
+    this.cost = 0;
   };
 
-function selectPizza(toppings) {
-  return toppings.sort(function (pizzaPie) {
-    pizzaPie.type;
-  });
-}
-
-let toppings = [{type:'pineapple'}, {type: 'olive'}, {type: 'jalapeño'}];
-let type = selectPizza(toppings);
-
-function selectDimensions(size) {
-  return size.sort(function (width) {
-  width.ofPie;
-  });
-}
-let size = [{type: 'small'}, {type: 'medium'}, {type: 'large'}];
-let ofPie = selectDimensions(size);
-
 PizzaParty.prototype.sizeCost = function() {
-  if(this.size === small) {
+  if(this.size === "small") {
     this.cost += 10;
-  } else if (this.size === medium) {
+  } else if (this.size === "medium") {
     this.cost += 15;
-  } else if (this.size === large) {
+  } else if (this.size === "large") {
     this.cost += 20;
   }
 };
@@ -42,20 +23,20 @@ $(document).ready(function() {
     event.preventDefault();
   
   let selectedToppings = []
-  $("input:checkbox[name=form-one-input]:checked").each(function() {
+  $("input:checkbox[name=topping-input]:checked").each(function() {
     const formOne = $(this).val();
     selectedToppings.push(formOne);
   });
-  const selectedSize = $("input:checkbox[name=size]:checked").val();
-  let selectedPizza = new Pizza(selectedToppings, selectedSize);
-  let selectedPizzaCost = selectedPizza.pizzaCost();
+  
+  const selectedSize = $('#select-size');
+  let selectedPizza = new PizzaParty(selectedToppings, selectedSize);
+  selectedPizza.pizzaCost();
   
   $("#order-details").show();
-  $("button#order").click(function() {
-    $("ul").prepend("<li>20!</li>");
   let viewToppings = selectedToppings.join(",");  
   $(".size-choice").html(selectedSize);
   $(".topping-choice").html(viewToppings);
   $(".order-total").html(selectedPizzaCost);
   });
-});
+  });
+
